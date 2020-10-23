d4de86889f66f2e05c5891023b11fd00abe121f2
Dipesh Bajgain <dipbazz@gmail.com>
2020-10-23 14:14:38 +0545

    Add my_each_with_index method
    - used for loop to iterate over items
    - used a counter variable to track index value
    - used yield to return single item and index value

--- main.rb
+++ main.rb
@@ -2,6 +2,18 @@ module Enumerable
   def my_each
     yield self
   end
+
+  def my_each_with_index
+    counter = 0
+      # puts "#{value}: #{index}"
+    for item in self
+      yield item, counter
+      counter += 1
+    end
+  end
 end
 
-['John', 23, 90, 77, 'Foo'].my_each { |item| puts item }
+['John', 23, 90, 77, 'Foo'].my_each { |item| puts "#{item}" }
+
+
+['John', 23, 90, 77, 'Foo'].each_with_index { |item, index| puts "#{item}, #{index}" }
