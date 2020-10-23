8e2b3cebdd19d843068638b40493c38da8713f10
CY Kalu <cy23@tutanota.com>
2020-10-23 10:05:55 +0100

    Add .my_select method
    - fix bug with .my_each method

--- main.rb
+++ main.rb
@@ -1,19 +1,28 @@
 module Enumerable
   def my_each
-    yield self
+    for item in self
+      yield item
+    end
   end
 
   def my_each_with_index
     counter = 0
-      # puts "#{value}: #{index}"
     for item in self
       yield item, counter
       counter += 1
     end
   end
+
+  def my_select
+    selected = []
+    my_each {|item| selected.push(item) if yield(item)}
+    selected
+  end
 end
 
-['John', 23, 90, 77, 'Foo'].my_each { |item| puts "#{item}" }
+['John', 23, 90, 77, 'Foo'].my_each { |item| puts item }
 
+['John', 23, 90, 77, 'Foo'].my_each_with_index { |item, index| puts "#{item}, #{index}" }
 
-['John', 23, 90, 77, 'Foo'].each_with_index { |item, index| puts "#{item}, #{index}" }
+var = (0..10).my_select { |item| item.odd? }
+puts var
